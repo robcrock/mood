c8203e54459ca0f45b4a0baabf7d3d93f8befb6c
Robert Crocker <rcrocker13@gmail.com>
2024-11-06 09:51:31 -0600

    Add analysis.

--- src/app/(dashboard)/journal/[id]/page.tsx
+++ src/app/(dashboard)/journal/[id]/page.tsx
@@ -38,14 +38,42 @@ type PageProps = {
 
 export default async function JournalPage({ params }: PageProps) {
   const entry = await getEntry(params);
+  const anaysisData = [
+    { name: "Summary", value: "" },
+    { name: "Subject", value: "" },
+    { name: "Mood", value: "" },
+    { name: "Negative", value: "False" },
+  ];
 
   if (!entry) {
     return <div>Entry not found</div>;
   }
 
   return (
-    <div className="w-full h-full">
-      <Editor entry={entry} />
+    <div className="grid w-full h-full grid-cols-3">
+      <div className="col-span-2">
+        <Editor entry={entry} />
+      </div>
+      <div className="col-span-1 border-l border-black/10">
+        <div className="px-6 py-10 bg-blue-300">
+          <h2 className="text-2xl">Analysis</h2>
+        </div>
+        <div>
+          <ul>
+            {anaysisData.map((item) => {
+              return (
+                <li
+                  key={item.name}
+                  className="flex items-center justify-between px-2 py-4 border-t border-b border-black/10"
+                >
+                  <span className="text-lg font-semibold">{item.name}:</span>
+                  <span>{item.value}</span>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -17,13 +17,13 @@ type EditorProps = {
 };
 
 const Editor = ({ entry }: EditorProps) => {
-  if (!entry) {
-    return <div>Loading...</div>;
-  }
-
   const [text, setText] = useState(entry.content);
   const [currentEntry, setEntry] = useState<JournalEntry>(entry);
   const [isSaving, setIsSaving] = useState(false);
+
+  if (!entry) {
+    return <div>Loading...</div>;
+  }
 
   useAutosave({
     data: text,
@@ -44,21 +44,21 @@ const Editor = ({ entry }: EditorProps) => {
   });
 
   return (
-    <div className="relative grid w-full h-full grid-cols-3 gap-0">
-      <div className="absolute top-0 left-0 p-2">
+    <div className="relative w-full h-full overflow-hidden">
+      <div className="absolute top-0 left-0 z-10 p-2">
         {isSaving ? (
           <div>...saving</div>
         ) : (
           <div className="w-[16px] h-[16px] rounded-full bg-green-500"></div>
         )}
       </div>
-      <div className="col-span-2">
+      <div className="h-full">
         <textarea
           value={text}
           onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) =>
             setText(e.target.value)
           }
-          className="w-full h-full p-8 text-xl"
+          className="box-border w-full h-full p-8 text-xl resize-none"
         />
       </div>
     </div>
